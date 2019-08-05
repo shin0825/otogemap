b19a01490f4f76f0445f6b3fe92ba4154f287d37
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-08-05 23:25:12 +0900

    amusement_arcadesコントローラの追加・実装

---
+++ app/controllers/amusement_arcades_controller.rb
@@ -0,0 +1,54 @@
+class AmusementArcadesController < ApplicationController
+  before_action :finding_arcade, only: %i(edit update destroy)
+
+  def new
+    @arcade = AmusementArcade.new()
+  end
+
+  def show
+    @arcade = AmusementArcade.includes(:prefecture).find_by(id: params[:id])
+    @hash = Gmaps4rails.build_markers(@arcade) do |arcade, marker|
+      marker.lat arcade.latitude
+      marker.lng arcade.longitude
+      marker.infowindow arcade.name
+    end
+  end
+
+  def edit; end
+
+  def create
+    @arcade = AmusementArcade.new(arcade_params)
+    if @arcade.save
+      redirect_to @arcade, notice: 'created!!'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @arcade.update(arcade_params)
+      redirect_to @arcade, notice: 'updated!!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @arcade.destroy
+    redirect_to amusement_arcades_path, notice: 'ゲームセンター情報を削除しました'
+  end
+
+  private
+    def finding_arcade
+      @arcade = AmusementArcade.find_by(id: params[:id])
+    end
+
+    def arcade_params
+      params.require(:amusement_arcade).permit(
+        :name,
+        :name_kana,
+        :prefecture_id,
+        :address
+      )
+    end
+end
--- app/models/amusement_arcade.rb
+++ app/models/amusement_arcade.rb
@@ -1,4 +1,31 @@
 class AmusementArcade < ApplicationRecord
-  validates :prefecture_id, presence: true
   belongs_to :prefecture
+
+  before_validation :set_prefecture
+  validates :prefecture_id, presence: true
+
+  after_validation :geocode
+
+  private
+  def geocode
+    uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"&key=" + ENV['GCP_API_KEY'])
+    res = HTTP.get(uri).to_s
+    response = JSON.parse(res)
+    if response["status"] == 'OK'
+      self.latitude = response["results"][0]["geometry"]["location"]["lat"]
+      self.longitude = response["results"][0]["geometry"]["location"]["lng"]
+    else
+      self.latitude = 0
+      self.longitude = 0
+    end
+  end
+
+  def set_prefecture
+    self.prefecture_id = nil
+    prefecture_name = self.address.match(/.*[都道府県]/)
+    if prefecture_name
+      prefecture = Prefecture.find_by(name: prefecture_name[0])
+      self.prefecture_id = prefecture.id
+    end
+  end
 end
