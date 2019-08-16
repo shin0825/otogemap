7399e645b8ca4d7b7461c72059cdd552377c5a78
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-08-16 17:48:39 +0900

    seed 修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 # Prefecture
-# Prefecture.create(:id => 21, :name => "岐阜県", :name_kana => "ギフケン")
-# Prefecture.create(:id => 22, :name => "静岡県", :name_kana => "シズオカケン")
-# Prefecture.create(:id => 23, :name => "愛知県", :name_kana => "アイチケン")
-# Prefecture.create(:id => 24, :name => "三重県", :name_kana => "ミエケン")
+Prefecture.create(:id => 21, :name => "岐阜県", :name_kana => "ギフケン")
+Prefecture.create(:id => 22, :name => "静岡県", :name_kana => "シズオカケン")
+Prefecture.create(:id => 23, :name => "愛知県", :name_kana => "アイチケン")
+Prefecture.create(:id => 24, :name => "三重県", :name_kana => "ミエケン")
 
 # AmusementArcade
-# AmusementArcade.create(
-#   :name => "わたしのゲームセンター",
-#   :name_kana => "ワタシノゲームセンター",
-#   :prefecture_id => 23,
-#   :address => "愛知県名古屋市中川区",
-#   :latitude => 35.1405944,
-#   :longitude => 136.8534181,
-# )
+AmusementArcade.create(
+  :name => "コムテックタワー",
+  :name_kana => "こむてっくたわー",
+  :prefecture_id => 23,
+  :address => "愛知県名古屋市中村区名駅４丁目１０−２０",
+  :latitude => 35.169398,
+  :longitude => 136.885995,
+)
