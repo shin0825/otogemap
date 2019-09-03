b9289f025a49a947814074de7ebd9adf419fc255
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-09-03 23:06:56 +0900

    Google Mapの表示をAPIを使わない形に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   include AjaxErrorRenderer
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :allow_googlemap_iframe
+
+  GOOGLE_MAP_URL = "https://maps.google.co.jp/maps"
 
   # このアクションを追加
   def after_sign_in_path_for(resource)
@@ -12,5 +15,12 @@ class ApplicationController < ActionController::Base
   # 入力フォームからアカウント名情報をDBに保存するために追加
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  private
+
+  def allow_googlemap_iframe
+    response.headers['X-Frame-Options'] = "ALLOW-FROM #{GOOGLE_MAP_URL}"
+    response.headers['Content-Security-Policy'] = "frame-ancestors #{GOOGLE_MAP_URL}"
   end
 end
