91700247e66639db26af1a29f0a65605dcc95ea3
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-09-03 23:13:52 +0900

    ルートディレクトリから検索可能にした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   # このアクションを追加
   def after_sign_in_path_for(resource)
-    "/user/#{current_user.id}"
+    "/"
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "amusement_arcades#index"
+  root to: "static_pages#home"
 
   devise_for :users, :controllers => {
     :sessions => "users/sessions"
